3f715a6c2243774cf8241cd9433839a741114a2a
Nguyen Huy <ironsight@ironsight.local>
2018-03-04 15:08:27 +0700

    add enable flag for transfer

--- contracts/SpoutMintableToken.sol
+++ contracts/SpoutMintableToken.sol
@@ -6,4 +6,30 @@ contract SpoutMintableToken is MintableToken {
   string public constant name = "SpoutToken";
   string public constant symbol = "SPT";
   uint8 public constant decimals = 18;
+  address originalOwner;
+
+  bool public transferEnabled = false;
+
+  function SpoutMintableToken() public {
+    originalOwner = msg.sender;
+  }
+
+  function setTransferStatus(bool _enable)  public {
+    require(originalOwner == msg.sender);
+    transferEnabled = _enable;
+  }
+
+  function getTransferStatus() public view returns (bool){
+      return transferEnabled;
+  }
+
+  function getOriginalOwner() public view returns(address) {
+      return originalOwner;
+  }
+
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(transferEnabled);
+
+    return super.transfer(_to, _value);
+  }
 }
